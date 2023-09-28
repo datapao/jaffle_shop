e0f6a17a464fbee58b3c2187bd8d5ebb938a31a7
Tamás Sebők <tamas.sebok@databricks.com>
2023-09-28 14:55:56 +0200

    Update exec_py.py

--- exec_py.py
+++ exec_py.py
@@ -4,10 +4,16 @@ import subprocess
 
 print("From python file in git")
 print('Hello world', file=sys.stderr)
-os.system("python /user/home/main.py")
+proc = subprocess.run(["python", "/user/home/main.py"], capture_output=True, shell=True)
+print("Script output:", proc.stdout)
+print("Script error:", proc.stderr)
 print("Python script from docker executed")
 
 proc = subprocess.run(["dbt", "--version"], capture_output=True, shell=True)
 print("DBT output:", proc.stdout)
 print("DBT error:", proc.stderr)
 print("DBT run called")
+
+print("Env vars:")
+for name, value in os.environ.items():
+    print("{0}: {1}".format(name, value))
