768b47e524a3d2e9945cee4cecdc413cd8dcd7ee
Tamás Sebők <tamas.sebok@databricks.com>
2023-09-28 15:43:30 +0200

    Update exec_py.py

--- exec_py.py
+++ exec_py.py
@@ -9,9 +9,17 @@ with open("/user/home/main.py", "r") as file:
     for line in file:
         print(line.rstrip())
 
-proc = subprocess.run(["python", "/user/home/main.py"], capture_output=True, shell=True)
+proc = subprocess.run(["which", "python"], capture_output=True, shell=True)
 print("Script output:", proc.stdout)
 print("Script error:", proc.stderr)
+
+proc = subprocess.run(["python", "--version"], capture_output=True)
+print("Script output:", proc.stdout)
+print("Script error:", proc.stderr)
+
+#proc = subprocess.run(["python", "/user/home/main.py"], capture_output=True, shell=True)
+#print("Script output:", proc.stdout)
+#print("Script error:", proc.stderr)
 print("Python script from docker executed")
 
 proc = subprocess.run(["dbt", "--version"], capture_output=True, shell=True)
