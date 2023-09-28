36cc33f48f1d5c5574ec5f442ba3f9ddd44eb802
Tamás Sebők <tamas.sebok@databricks.com>
2023-09-28 13:58:13 +0200

    Update exec_py.py

--- exec_py.py
+++ exec_py.py
@@ -1,3 +1,10 @@
 import os
+import sys
 
+print("From python file in git")
+print('Hello world', file=sys. stderr)
 os.system('python /user/home/main.py')
+print("Python script from docker executed")
+print('Executed (on error)', file=sys. stderr)
+os.system("dbt run")
+print("DBT run called")
